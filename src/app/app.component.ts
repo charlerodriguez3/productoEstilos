1c9e0e85342fd4a24fe4e09a1b05bcefac0a7366
Carlos A Rodriguez <andres199924@hotmail.com>
2025-06-17 15:59:02 -0500

    adding ng-zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NzButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
